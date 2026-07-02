a4eb4547415be0878daea7b30577ead2e7a0b175
hareCat <harecatx@gmail.com>
2026-07-02 18:02:39 +0700

    Initial commit

---
+++ src/test/java/com/iplion/kinopoisk/KinopoiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iplion.kinopoisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinopoiskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
